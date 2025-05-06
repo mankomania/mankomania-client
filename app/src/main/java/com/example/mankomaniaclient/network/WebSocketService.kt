54d53f9aec710f2f9759e1d61384016d8fe5db6c
Angie Drucks <angie.drucks@gmail.com>
2025-05-06 18:08:28 +0200

    test: deleted an unused import

--- app/src/main/java/com/example/mankomaniaclient/network/WebSocketService.kt
+++ app/src/main/java/com/example/mankomaniaclient/network/WebSocketService.kt
@@ -4,7 +4,6 @@ import android.util.Log
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import org.hildan.krossbow.stomp.StompClient
 import org.hildan.krossbow.stomp.StompSession
