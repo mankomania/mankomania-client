d09f6d664d63f98a0642703cda275a137237762d
Valentina Schiavon <valentinaschiavon99@gmail.com>
2025-05-09 17:01:04 +0200

    refactor: align build.gradle with main project standards and migrate to kotlinx.serialization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21"
     id("jacoco")
     id("org.sonarqube") version "5.1.0.4882"
 }
@@ -142,4 +143,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.kotlinx.serialization.json)
 }
