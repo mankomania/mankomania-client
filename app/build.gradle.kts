8ebecc2a95f5c7e17cfa2e848c9c6fa5adaad7dc
Anna Walder <annapalomawa@edu.aau.at>
2025-05-10 21:19:07 +0200

    added Rule button and rules to the name screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -140,6 +140,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.constraintlayout)
+    implementation("androidx.appcompat:appcompat:1.6.1")
     testImplementation(libs.junit)
     testImplementation(libs.junit.jupiter.api)
     testImplementation(libs.junit.jupiter.engine)
