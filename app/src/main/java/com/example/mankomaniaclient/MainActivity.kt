06b16e74a121b7f2d2d0100d5fcfdbfd329f275b
vanidrei <vanfroehlich@edu.aau.at>
2025-05-10 14:08:16 +0200

    fix: added import GameActivity.kt

--- app/src/main/java/com/example/mankomaniaclient/MainActivity.kt
+++ app/src/main/java/com/example/mankomaniaclient/MainActivity.kt
@@ -47,6 +47,7 @@ import android.content.Intent
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
+import com.example.mankomaniaclient.com.example.mankomaniaclient.GameActivity
 
 
 import com.example.mankomaniaclient.network.WebSocketService
