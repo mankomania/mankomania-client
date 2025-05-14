ce101c88f241cbd5260e9714ec4724cce785fb70
eles17 <lev.starman8@gmail.com>
2025-05-14 18:28:38 +0200

    feat(PlayerStatus): create PlayerStatus data class for real-time status updates (#10)
    - Represents live player data received via WebSocket (/topic/player/{name}/status)
    - Includes name, position, balance, and denomination map
    - Placed in model package for clean structure

---
+++ app/src/main/java/com/example/mankomaniaclient/model/PlayerStatus.kt
@@ -0,0 +1,15 @@
+package com.example.mankomaniaclient.model
+
+/**
+ *
+ * @author eles17
+ * @since 14.5.2025
+ * @description
+ * Implement real-time status updates for each player using WebSocket messages and reflect them in the UI.
+ */
+data class PlayerStatus(
+    val name: String,
+    val position: Int,
+    val balance: Int,
+    val money: Map<Int, Int>
+)
